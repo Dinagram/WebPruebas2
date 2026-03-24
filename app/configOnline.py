7a7f9c59efb65271650cde4ed702ef6983c8da0d
Dinagram <robot@dinagram.es>
2026-03-24 17:57:47 +0100

    Delete app/configOnline.py

--- app/configOnline.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class ConfigOnline:
-    SECRET_KEY = 'Bioweh??3jiqwgf2'
-    CLIENT_ACCESS_TOKEN_MAX_AGE = 3600
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "postgresql://datosweb_ynfl_user:Z5mpVwVboQMm41tTgWhGsSBpTWKS6D3l@dpg-d717uljuibrs73aaajug-a.frankfurt-postgres.render.com:5432/datosweb_ynfl"
